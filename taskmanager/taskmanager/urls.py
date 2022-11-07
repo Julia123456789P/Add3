7437708c67919fab028aabbc43c47bed7ef9490b
Yuliya Skriba <julia.life.net@gmail.com>
2022-11-07 18:10:09 +0300

    new email

--- taskmanager/taskmanager/urls.py
+++ taskmanager/taskmanager/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
@@ -10,6 +11,7 @@ urlpatterns = [
     path('main/', include('main.urls')),
     path('', include('shop.urls', namespace='shop')),
 ] #TODO ссылки на все приложения
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
